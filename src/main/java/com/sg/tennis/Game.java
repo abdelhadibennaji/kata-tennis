57f7386f7b9a910b1e70fd93870a534349299509
abdelhadibennaji <42415726+abdelhadibennaji@users.noreply.github.com>
2020-07-26 15:38:01 +0200

    Feature/sp1 us1 (#1)
    User story 1

---
+++ src/main/java/com/sg/tennis/Game.java
@@ -0,0 +1,22 @@
+package com.sg.tennis;
+
+public class Game {
+
+
+    private Game(Player firstPlayer, Player secondPlayer) {
+
+    }
+
+    public static Game start(Player firstPlayer, Player secondPlayer) {
+        return new Game(firstPlayer, secondPlayer);
+    }
+
+
+    public Score getScoreFirstPlayer() {
+        return Score.ZERO;
+    }
+
+    public Score getScoreSecondPlayer() {
+        return Score.ZERO;
+    }
+}
---
+++ src/main/java/com/sg/tennis/Player.java
@@ -0,0 +1,10 @@
+package com.sg.tennis;
+
+public class Player {
+
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+}
