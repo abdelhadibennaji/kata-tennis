f66308e2c18d3406237e451d44b6efea4da9c2cf
Abdelhadi BENNAJI <abdelhadi.bennaji@gmail.com>
2020-07-26 14:38:23 +0200

    Test initial game score : faild

---
+++ src/test/java/com/sg/tennis/GameTest.java
@@ -0,0 +1,20 @@
+package com.sg.tennis;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class GameTest {
+
+    @Test
+    public void The_game_should_starts_with_a_score_of_0_point_for_each_player() {
+        // GIVEN
+        Player firstPlayer = new Player("Nadal");
+        Player secondPlayer = new Player("Jeko");
+        // WHEN
+        Game game = Game.start(firstPlayer, secondPlayer);
+        // THEN
+        assertEquals(0, game.getScoreFirstPlayer());
+        assertEquals(0, game.getScoreSecondPlayer());
+    }
+}
